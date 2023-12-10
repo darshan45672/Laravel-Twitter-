91b123b903f5bf3dad9a7a98f191a4c88884b425
darshan45672 <drshnbhandary@gmail.com>
2023-12-10 16:35:38 +0530

    added 2 controllers ProfileControllers and Dashbaord Controllers also added routing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,23 +6,7 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    // 
     public function index(){
-
-        $users = [
-            [
-                'name' => 'Darshan',
-                'age' => 21,
-            ],
-            [
-                'name' => 'Nidhi',
-                'age' => 21,
-            ]
-        ];
-    
-        return view('dashboard',
-        [
-            'users' => $users
-        ]);
+        return view("welcome");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +15,5 @@ use App\Http\Controllers\ProfileController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/',[DashboardController::class , 'index']);
-Route::get('/profile',[ProfileController::class , 'index']);
+Route::get('/', [ DashboardController::class, 'index']);
+Route::get('/profile', [ ProfileController::class, 'index']);
