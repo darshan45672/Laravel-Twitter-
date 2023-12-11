5631389ddddc2e5fbb4febf6cbfdad09b0b1a754
darshan45672 <drshnbhandary@gmail.com>
2023-12-11 21:09:47 +0530

    created model Idea and integrated with view, added layouts in views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -53,6 +53,7 @@
                 <h5 class="">Search</h5>
             </div>
             <div class="card-body">
+                
                 <input placeholder="...
                         " class="form-control w-100" type="text" id="search">
                 <button class="btn btn-dark mt-2"> Search</button>
