3ee5819f5f58e92ba4b1db0010643b49a03a29a5
darshan45672 <drshnbhandary@gmail.com>
2024-01-17 20:26:11 +0530

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -48,4 +48,4 @@ Route::get('/terms', function () {
     return view('terms');
 })->name('terms');
 
-Route::get('/admin', [AdminDashboardController::class, 'index'])->middleware(['auth','admin'])->name('admin.dashboard');
+Route::get('/admin', [AdminDashboardController::class, 'index'])->middleware(['auth','can:admin'])->name('admin.dashboard');
