3f256a1e27bf9db0fd801d4b292002884870060f
darshan45672 <drshnbhandary@gmail.com>
2024-01-19 20:16:20 +0530

    added auth  middleware to logout  route auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -4,13 +4,12 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware'=>'guest'],function(){
-
+    
     Route::get('/register', [AuthController::class, 'register'])->name('register');
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     
     Route::post('/register', [AuthController::class, 'store']);
     Route::post('/login', [AuthController::class, 'authenticate']);
-    
 });
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
