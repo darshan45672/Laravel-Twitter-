c3c73f810cd3c58c5b4a685b86282ced1cf406d2
darshan45672 <drshnbhandary@gmail.com>
2024-01-19 20:10:14 +0530

    added guest middleware to auth.php and updated route service provider HOME

--- routes/auth.php
+++ routes/auth.php
@@ -3,9 +3,14 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::group(['middleware'=>'guest'],function(){
 
-Route::post('/register', [AuthController::class, 'store']);
-Route::post('/login', [AuthController::class, 'authenticate']);
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    
+    Route::post('/register', [AuthController::class, 'store']);
+    Route::post('/login', [AuthController::class, 'authenticate']);
+    
+});
+
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
